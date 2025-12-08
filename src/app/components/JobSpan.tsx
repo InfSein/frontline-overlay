6b34801ed2adf76758a774858aed49abce9a6a89
InfSein <infsein@foxmail.com>
2025-12-08 20:34:48 +0800

    1

--- src/app/components/JobSpan.tsx
+++ src/app/components/JobSpan.tsx
@@ -17,17 +17,21 @@ const JobSpan : React.FC<JobSpanProps> = ({
     job_name: jobName
   } = getJobInfo(job)
 
-  const _size = size || '1.1em'
+  const _size = size || '1.25rem'
 
-  const imgClassName = `inline-block w-[${_size}] h-[${_size}] vertical-align:middle`
+  const imgClassName = `inline-block vertical-align:middle`
+  const imgStyle = {
+    width: _size,
+    height: _size,
+  }
 
   return showJobName ? (
     <div className="flex items-center gap-1 text-[1.1rem] text-gray-200 leading-[1] m-0 text-shadow">
-      <img src={`./icon/game-job/${jobIcon}.png`} className={imgClassName} />
+      <img src={`./icon/game-job/${jobIcon}.png`} className={imgClassName} style={imgStyle} />
       { jobName }
     </div>
   ) : (
-    <img src={`./icon/game-job/${jobIcon}.png`} className={imgClassName} />
+    <img src={`./icon/game-job/${jobIcon}.png`} className={imgClassName} style={imgStyle} />
   )
 }
 
